9d35a61b8fefdd6a2fbd3171ff6102068cec045c
yogesh raheja <yogesh.raheja@thinknyx.com>
2019-03-08 06:39:46 +0000

    first file commited

---
+++ nishu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Second project build"
+apt-get update
+mkdir /tmp/sumit
+touch /tmp/sumit/sumit1
+echo "first file" > /tmp/sumit/sumit1
+touch /tmp/sumit/sumit2
+echo "second file" > /tmp/sumit/sumit2
+echo "Project done"
